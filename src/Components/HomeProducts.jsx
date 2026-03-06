d44c4e0dbff11a393b69cd2b17270340102308af
amal nath <amaltq10@gmail.com>
2026-03-06 18:56:45 +0530

    Home collection page   06  03 2026

--- src/Components/HomeProducts.jsx
+++ src/Components/HomeProducts.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getMethod } from '../ApiServices/autoMethod'
 import { urlmainType, urlproduct } from '../ApiServices/setUrl'
+import '../Sryling/HomeProduct.css'
 
 function HomeProducts() {
     const [product,setProduct]=useState([])
@@ -41,8 +42,8 @@ function HomeProducts() {
             return el.subType
           })
 
-          console.log(temp)
-    },[product,mainCtgry,wholeC,formData?.MCategory])
+          console.log(formData)
+    },[product,mainCtgry,wholeC,formData?.MCategory,formData])
 
 
 
@@ -51,11 +52,16 @@ function HomeProducts() {
         setFormData({...formData,[name] : value})
         console.log(formData.MCategory)
         console.log(formData.SubCategory)
+        
     }
   return (
     <>
    <form >
-    <select name="MCategory" required onChange={handleChange}>
+    <select name="MCategory"  required onChange={(e)=>{handleChange(e);
+       
+        setFormData(prev=>({...prev,SubCategory:""}))
+
+    }}>
         <option value="" selected >Choose main category</option>
         {
             mainCtgry.map((el,i)=>{
@@ -64,14 +70,16 @@ function HomeProducts() {
             })
         }
     </select>
-    <select name="SubCategory" onChange={handleChange} >
-        <option value="" selected required >Choose Sub category</option>
+    <select name="SubCategory" value={formData.SubCategory} required onChange={(e)=>{handleChange(e);
+       
+    }} disabled={!formData.MCategory} >
+        <option value=""   >Choose Sub category</option>
         {
          wholeC.find((el,i)=>{
            
             return el?.[formData?.MCategory]
           })?.[formData?.MCategory].map((el,i)=>{
-           return <option value={el.subType} >{el.subType}</option>
+           return <option key={i} value={el.subType} >{el.subType}</option>
             
           })
           
@@ -79,26 +87,63 @@ function HomeProducts() {
         }
 
     </select>
-    <table>
-        <thead>
+   
+  
+
+    
+     <table className='table-hmprdct' >
+       <thead>
             <tr>
                 <th>sl.no</th>
                 <th>Id</th>
                 <th>product</th>
+                <th>price</th>
                 <th>Image </th>
                 <th>add </th>
                 <th>remove </th>
 
             </tr>
         </thead>
-        <tbody>
-            {
-              product.filter((el,i)=>{
-                return el.category.T1===formData.MCategory && el.category.T2===formData.SubCategory
+      <tbody>
+             {
+              formData.MCategory&&!formData.SubCategory&& product.filter((el,i)=>{
+                return el.category.T1===formData.MCategory 
+              })?.map((el,i)=>{
+                console.log(el.image)
+                return <tr key={i}>
+                    <td>{i+1}</td>
+                    <td>{el.id}</td>
+                    <td>{el.name}</td>
+                    <td>{el.price}</td>
+                    <td style={{textAlign:"center"}}><img src={el.image} style={{ width:"100px",aspectRatio:"16/9",objectFit:"contain",objectPosition:"center"}}/></td>
+                    <td><button>Add</button></td>
+                    <td><button>Remove</button></td>
+                </tr>
               })
             }
+             {
+
+             formData.MCategory && formData.SubCategory && product.filter((el,i)=>{
+                return el.category.T1===formData.MCategory && el.category.T2===formData.SubCategory
+              })?.map((el,i)=>{
+                console.log(el.image)
+                return <tr key={i}>
+                    <td>{i+1}</td>
+                    <td>{el.id}</td>
+                    <td>{el.name}</td>
+                    <td>{el.price}</td>
+                    <td style={{textAlign:"center"}}><img src={el.image} style={{ width:"100px",aspectRatio:"16/9",objectFit:"contain",objectPosition:"center"}}/></td>
+                    <td><button>Add</button></td>
+                    <td><button>Remove</button></td>
+                </tr>
+              })
+            }
+        
         </tbody>
+       
     </table>
+
+    
    </form>
     </>
   )
