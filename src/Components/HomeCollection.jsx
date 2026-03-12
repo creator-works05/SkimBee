0ed82c2a57cd5ef27527d08cc21ccb5183bb8211
amal nath <amaltq10@gmail.com>
2026-03-12 13:25:48 +0530

    Home collection page new  0443pm  12  03 2026

--- src/Components/HomeCollection.jsx
+++ src/Components/HomeCollection.jsx
@@ -36,68 +36,147 @@ function HomeCollection() {
                 let j=i+1;
                 if(j<=4){
                     if(j===1){
-                        return <div className='big-hmclctn'></div>
+                        return <div className='big-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
                     }
                     if(j===2){
-                        return <div className='small-hmclctn'></div>
+                        return <div className='small-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
 
                     }
                     if(j===3){
-                        return <div className='big-hmclctn'></div>
+                        return <div className='big-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
 
                     }
                     if(j===4){
 
-                        return <div className='flex-hmclctn' style={{gridColumn:"span 7"}}></div>
+                        return <div className='flex-hmclctn' style={{gridColumn:"span 4"}}>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
                     }
                 }
                 else if(j<=8){
                      if(j===5 || j===6){
-                        return <div className='mid-hmclctn'></div>
+                        return <div className='mid-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
                     }
                     if(j===7){
-                        return <div style={{gridColumn:"span 7"}} className='flex-hmclctn'></div>
+                        return <div style={{gridColumn:"span 4"}} className='flex-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
 
                     }
                     if(j===8){
 
-                        return <div className='big-hmclctn'></div>
+                        return <div className='big-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
                     }
                 }
                 else if(j<=12){
                      if(j===9|| j===12){
-                        return <div className='small-hmclctn'></div>
+                        return <div className='small-hmclctn'>
+                             <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+
+                        </div>
                     }
                     if(j===10||j===11){
-                        return <div style={{gridColumn:"span 7"}} className='flex-hmclctn'></div>
+                        return <div style={{gridColumn:"span 6"}} className='flex-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
 
                     }
                     
                 }
                 else if(j<=16){
-                     if(j===13 ||j===16 ){
-                        return <div className='big-hmclctn'></div>
+                     if(j===13  ){
+                        return <div className='big-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
                     }
-                    if(j===14){
-                        return <div className='mid-hmclctn'></div>
+                    if(j===14 ||j===16){
+                        return <div className='mid-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+
+                        </div>
 
                     }
                     if(j===15){
 
-                        return <div style={{gridColumn:"span 6"}} className='flex-hmclctn'></div>
+                        return <div style={{gridColumn:"span 4"}} className='flex-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
                     }
                 }
                 else{
                      if(j===17||j===20){
-                        return <div className='big-hmclctn'></div>
+                        return <div className='big-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
                     }
                     if(j===18){
-                        return <div style={{gridColumn:"span 7"}} className='flex-hmclctn'></div>
+                        return <div style={{gridColumn:"span 4"}} className='flex-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+                        </div>
 
                     }
                     if(j===19){
 
-                        return <div className='small-hmclctn'></div>
+                        return <div className='small-hmclctn'>
+                            <img src={el.image} />
+                            <h4>{el.name}</h4>
+                            <p>{el.desc}</p>
+                           <h5>₹ {el.price}</h5>
+
+                            
+                        </div>
                     }
                 }
 
